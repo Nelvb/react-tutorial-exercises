d31a64331f856dc11457f10a4682768972f53e1c
Nelvb <169047839+Nelvb@users.noreply.github.com>
2024-07-30 19:22:31 +0000

    finalizado

--- .learn/resets/03.5-alert-component/app.jsx
+++ .learn/resets/03.5-alert-component/app.jsx
@@ -2,21 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-const Alert = (props) => {
-	return (
-		<div className="alert alert-danger" role="alert">
-			{props.text}
-		</div>
-	);
-};
 /**
  * Here you have to declare your Alert component and return
  * the html that bootstrap dictates for its alert component
  */
-<Alert text="OMG! Something really bad has happened!" />;
-Alert.propTypes = {
-	text: PropTypes.string.isRequired,
-};
+
 
 // Here is where the <Alert /> component is being used, you don't have to edit this part
 // But it helps you understand what properties the component is using
--- .learn/resets/05.1-second-styles/app.jsx
+++ .learn/resets/05.1-second-styles/app.jsx
@@ -3,20 +3,16 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 const buttonStyles = {
-	background: "yellow",
-	color: "black",
-	border: "none",
-
 	// Write button styles here
+
 };
 
 const badgeStyles = {
 	// Write the span styles here
-	background: "red",
-	borderRadius: "50%",
+
 };
 
-const Badge = (props) => {
+const Badge = props => {
 	return (
 		<button style={buttonStyles} type="button" className="btn btn-primary">
 			{props.label}
@@ -29,7 +25,7 @@ const Badge = (props) => {
 
 Badge.propTypes = {
 	label: PropTypes.string,
-	number: PropTypes.string,
+	number: PropTypes.string
 };
 
 ReactDOM.render(<Badge label="Alerts" number="2" />, document.querySelector("#myDiv"));
